c64baf37205addd3545a7cf9409a1138c79dc235
@rushi0077 <rwadkar@whr.loans>
2021-09-24 15:46:53 +0530

    Demo user validator process deleted

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -4,25 +4,6 @@ import {validationResult} from 'express-validator';
 
 export class UserController{
     static login(req, res, next){
-
-        const error = validationResult(req);
-
-        const email = req.body.email; 
-
-        const password = req.body.password;
-
-        const userName = req.body.userName;
-
-        const user = new User({email: email, password: password, userName: userName});
-
-        if(!error.isEmpty()){
-            const newError = new Error(error.array()[0].msg);
-
-            return(newError);
-        }
+        
     }
-
-    // static test(req, res, next){
-    //     console.log('called');
-    // }
 }
